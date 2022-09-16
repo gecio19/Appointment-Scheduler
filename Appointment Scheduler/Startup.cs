7f934289d82598ea1213bd5033979169ccfe78fc
gecio19 <dageron92@interia.eu>
2022-09-16 21:36:41 +0200

    First Commit

---
+++ Appointment Scheduler/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Appointment_Scheduler.Startup))]
+namespace Appointment_Scheduler
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
